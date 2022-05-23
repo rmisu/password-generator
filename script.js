88b83235e1750ffdfea373a239950dbdf957d6c0
rmisu <roger889@umn.edu>
2022-05-22 23:03:06 -0500

    clean up/add comments

--- script.js
+++ script.js
@@ -14,36 +14,42 @@ passwordLength = prompt("How long would you like your password to be? (Must be b
       passwordLength = prompt("Please choose a number between 8-128.")}
     else {alert("The generated password will be " + passwordLength + " characters long.")}
 
+// Determines if lowercase characters will be used
 useLowercase = confirm("Would you like your password to contain lowercase characters?");
     if (useLowercase) {alert("Lowercase characters will be included.")}
     else {alert("Lowercase characters will not be included.")}
 
+// Determines if UPPERCASE characters will be used
 useUppercase = confirm("Would you like your passwork to contain uppercase characters?");
     if (useUppercase) {alert("Uppercase characters will be included.")}
     else {alert("Uppercase characters will not be included.")}
 
+// Determines if numbers will be used
 useNumbers = confirm("Would you like your password to contain numbers?");
     if (useNumbers) {alert("Numbers will be included.")}
     else {alert("Numbers will not be included.")}
 
+// Determines if special characters will be used
 useSpecial = confirm("Would you like your password to containt special characters?");
     if (useSpecial) {alert("Special characters will be included.")}
     else {alert("Special characters will not be included.")}
-  
-    if (useLowercase === false && useUppercase === false && useNumbers === false && useSpecial === false) {
-      return "You must choose at least one type of character."};
 
-    if (useLowercase) {possibleCharacters = possibleCharacters.concat(lowercaseCharacters);}
-    if (useUppercase) {possibleCharacters = possibleCharacters.concat(uppercaseCharacters);}
-    if (useNumbers) {possibleCharacters = possibleCharacters.concat(numericCharacters);}
-    if (useSpecial) {possibleCharacters = possibleCharacters.concat(specialCharacters);}
+// Validation of selected criteria, condition that one must be chosen
+if (useLowercase === false && useUppercase === false && useNumbers === false && useSpecial === false) {
+      return "You must choose at least one type of character."}
+
+// Inclusion of selected character into possible characters
+if (useLowercase) {possibleCharacters = possibleCharacters.concat(lowercaseCharacters)}
+if (useUppercase) {possibleCharacters = possibleCharacters.concat(uppercaseCharacters)}
+if (useNumbers) {possibleCharacters = possibleCharacters.concat(numericCharacters)}
+if (useSpecial) {possibleCharacters = possibleCharacters.concat(specialCharacters)}
   
-// logic for final password
+// logic for final password generation given conditions selected above
     let finalPassword = ""
     for (let i = 0; i < passwordLength; i++) {
       let rng =[Math.floor(Math.random() * possibleCharacters.length)];
       finalPassword = finalPassword + possibleCharacters[rng]};
-    return finalPassword};
+    return finalPassword}
   
   // Get references to the #generate element
   var generateBtn = document.querySelector("#generate");
